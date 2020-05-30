075ad0000f492d09482848a01890753dca24b85b
Ellis Clarke <ellisbclarke03@gmail.com>
2020-05-30 18:01:20 +0100

    Update login.vb

--- NEA_DiceGame/login.vb
+++ NEA_DiceGame/login.vb
@@ -1,4 +1,5 @@
 ﻿Public Class login
+    'This code must not be copied but only used as a guide to write your own code 
     Private Sub Login_BTN_Click(sender As Object, e As EventArgs) Handles Login_BTN.Click
         If User_TXB.Text = "user" And Pass_TXB.Text = "Password1" Then
             game.Show()
